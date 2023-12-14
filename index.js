ef2e8ac4ff64b8309554e8273c7892f29d091579
duongdx <dinh.xuan.duong@sun-asterisk.com>
2023-12-14 23:28:42 +0700

    edit content

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
     const containerName = os.hostname();
     console.log('hello root')
     response.json({
-        service: "Duongdx edit 2023/12/14 22:58",
+        service: "Duongdx v7 edit 2023/12/14 23:15",
         contact: "xuanduong.kma@gmail.com",
         clientIp: clientIp,
         elbIp: elbIp,
