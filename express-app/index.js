21acc843b1ed315ba9d6c4d760ac0cf8c1dbdd6d
duongdx <dinh.xuan.duong@sun-asterisk.com>
2023-12-22 00:39:38 +0700

    multi application

--- express-app/index.js
+++ express-app/index.js
@@ -1,12 +1,14 @@
 const os = require('os')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8088;
 const users = require('./routes/user.route');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(
     bodyParser.urlencoded({
         extended: true,
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import logo from './logo.svg';
+import './App.css';
 import { useEffect, useState } from "react";
-
+import axios from "axios";
 
 function App() {
-  useEffect(() => {
+  const[users, setUsers] = useState([]);
 
+    const fetchPost = async () => {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept-Language': 'fr',
+            // 'Authorization': `Bearer ${accessToken}`,
+        }
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/users`, {headers});
+            setUsers(response?.data?.data)
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+  useEffect( () => {
+      fetchPost().then(r => console.log(r))
   }, [])
+
+    const render = useCallback(() => {
+    return users && users.map((user, index) => {
+      return (
+          <li key={index}>
+            <span>{user.user_id}</span>
+            :
+            <span>{user.user_name}</span>
+          </li>
+      )
+    })
+  }, [users])
 
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>CODE</code> and save to reload.
+          Edit <code>CODE</code> and save to reloadddd
         </p>
+        <ul>
+            {render()}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
